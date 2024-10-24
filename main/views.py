af85d66144f08597af1873e987e16eb08dc1d385
Farhan Khalid <rj.farhan4232@gmail.com>
2024-10-24 22:45:45 +0500

    Added Searching in the Api

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from main import models, serializers
 
 # Create your views here.
@@ -8,3 +8,5 @@ from main import models, serializers
 class BookViewsets(viewsets.ModelViewSet):
     queryset = models.Book.objects.all()
     serializer_class = serializers.BookSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title", "author", "genre"]
